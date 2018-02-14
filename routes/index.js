a954764746912e709287ef270e4368abedeafd88
zoar <zoar@k-side.net>
2018-02-14 14:02:06 +0900

    Display list of directories

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const getDir = require('../modules/getdir')
+const config = require('../config.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: '画像確認ページ',
+    dirArray: getDir(config.photodir)
+  });
 });
 
 module.exports = router;
