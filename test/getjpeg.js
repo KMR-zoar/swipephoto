9a8949ccc2dbd52709bee418d83bbcd845a7b6f0
zoar <zoar@k-side.net>
2018-02-21 15:53:38 +0900

    Test getjpeg function

---
+++ test/getjpeg.js
@@ -0,0 +1,11 @@
+
+const assert = require('assert');
+
+describe("GetJpegFileArray",() => {
+   it("getjepg",() => {
+      assert.equal(
+         "2016-03-14 08.24.40.jpg".toString(), 
+         jpegArray("photos/2016-03-14").toString()
+      )
+   });
+});
